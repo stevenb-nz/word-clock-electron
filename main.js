a0f5c9f30a7a712efbbd2a195b02825717bce14f
Steven Brown <sgbrown@mac.com>
2018-03-11 13:27:49 +1300

    sized window to match word-clock-xojo

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const path = require('path')
 const url = require('url')
 
   function createWindow () {
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 550, height: 242})
 
     win.loadURL(url.format({
       pathname: path.join(__dirname, 'index.html'),
