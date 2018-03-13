d3160f61b8a8f33d09674b76b9b369d69d94d20c
Steven Brown <sgbrown@mac.com>
2018-03-13 13:40:56 +1300

    refactor opening window (to avoid inadvertent garbage collection)

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
+'use strict';
+
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
-  function createWindow () {
-    win = new BrowserWindow({width: 550, height: 242})
+let win = null
 
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    }))
-  }
+app.once('ready', () => {
+  win = new BrowserWindow({
+    width: 550,
+    height: 242,
+    show: false
+  })
 
-  app.on('ready', createWindow)
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  win.once('ready-to-show', () => {
+    win.show()
+  })
+})
