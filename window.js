ea38fbab1e692a9e589acc09a58baccf6894228b
Steven Brown <sgbrown@mac.com>
2018-03-17 13:53:26 +1300

    get canvas context, draw text to canvas

--- window.js
+++ window.js
@@ -3,4 +3,10 @@ $(() => {
 
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+
+  var c = canvas.getContext("2d");
+
+  c.font = "30px Arial";
+  c.fillStyle = "#FF0000";
+  c.fillText("Hello World", 10, 50);
 })
