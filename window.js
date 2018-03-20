65d1552dda6215eade0525640098e1bd01caa255
Steven Brown <sgbrown@mac.com>
2018-03-20 19:12:37 +1300

    update current time every 5 minutes

--- window.js
+++ window.js
@@ -8,13 +8,25 @@ $(() => {
   var on = true;
 
   c.font = "24px System";
+
+  var d = new Date();
+  var c_hour = d.getHours() % 12;
+  var c_5min = Math.floor(d.getMinutes()/5)*5;
   window.setInterval(function() {
+    d = new Date();
+    if (c_hour !== d.getHours() % 12) {
+      c_hour = d.getHours() % 12;
+    }
+    if (c_5min !== Math.floor(d.getMinutes()/5)*5) {
+      c_5min = Math.floor(d.getMinutes()/5)*5;
+    }
     on = !on;
     if (on) {
       c.fillStyle = "#FF0000";
     } else {
       c.fillStyle = "#FF9999";
     }
-    c.fillText("HELLO WORLD", 40, 40);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.fillText(c_hour+' '+c_5min, 40, 40);
   }, 500);
 })
