443cc2ef6105356d927fa7e9364435d6893c15f1
Steven Brown <sgbrown@mac.com>
2018-03-21 16:25:43 +1300

    refactor to reduce repeated code

--- window.js
+++ window.js
@@ -1,4 +1,11 @@
 $(() => {
+  function extract_hour (d) {
+    return d.getHours() % 12;
+  }
+  function extract_5min (d) {
+    return Math.floor(d.getMinutes()/5)*5;
+  }
+
   var canvas = document.querySelector('canvas')
 
   canvas.width = window.innerWidth;
@@ -10,23 +17,25 @@ $(() => {
   c.font = "24px System";
 
   var d = new Date();
-  var c_hour = d.getHours() % 12;
-  var c_5min = Math.floor(d.getMinutes()/5)*5;
+  var c_hour = extract_hour(d);
+  var c_5min = extract_5min(d);
   window.setInterval(function() {
-    d = new Date();
-    if (c_hour !== d.getHours() % 12) {
-      c_hour = d.getHours() % 12;
-    }
-    if (c_5min !== Math.floor(d.getMinutes()/5)*5) {
-      c_5min = Math.floor(d.getMinutes()/5)*5;
-    }
     on = !on;
     if (on) {
       c.fillStyle = "#FF0000";
     } else {
       c.fillStyle = "#FF9999";
     }
-    c.clearRect(0, 0, canvas.width, canvas.height);
-    c.fillText(c_hour+' '+c_5min, 40, 40);
+    d = new Date();
+    if (c_hour !== extract_hour(d)) {
+      c_hour = extract_hour(d);
+      c.clearRect(0, 0, canvas.width, canvas.height);
+      c.fillText(c_hour+' '+c_5min, 40, 40);
+    }
+    if (c_5min !== extract_5min(d)) {
+      c_5min = extract_5min(d);
+      c.clearRect(0, 0, canvas.width, canvas.height);
+      c.fillText(c_hour+' '+c_5min, 40, 40);
+    }
   }, 500);
 })
