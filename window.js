08e88012c1929db8c3b4e118771bd258d4d03314
Steven Brown <sgbrown@mac.com>
2018-03-23 03:57:17 +1300

    refactor display code

--- window.js
+++ window.js
@@ -5,6 +5,10 @@ $(() => {
   function extract_5min (d) {
     return Math.floor(d.getMinutes()/5);
   }
+  function display_time (c_hour, c_5min, c) {
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.fillText(c_hour+' '+c_5min*5, 40, 40);
+  }
 
   var canvas = document.querySelector('canvas')
 
@@ -13,14 +17,13 @@ $(() => {
 
   var c = canvas.getContext("2d");
   var on = true;
-
+  c.fillStyle = "#FF0000";
   c.font = "24px System";
 
   var d = new Date();
   var c_hour = extract_hour(d);
   var c_5min = extract_5min(d);
-  c.clearRect(0, 0, canvas.width, canvas.height);
-  c.fillText(c_hour+' '+c_5min*5, 40, 40);
+  display_time(c_hour, c_5min, c);
 
   window.setInterval(function() {
     on = !on;
@@ -32,13 +35,11 @@ $(() => {
     d = new Date();
     if (c_hour !== extract_hour(d)) {
       c_hour = extract_hour(d);
-      c.clearRect(0, 0, canvas.width, canvas.height);
-      c.fillText(c_hour+' '+c_5min, 40, 40);
+      display_time(c_hour, c_5min, c);
     }
     if (c_5min !== extract_5min(d)) {
       c_5min = extract_5min(d);
-      c.clearRect(0, 0, canvas.width, canvas.height);
-      c.fillText(c_hour+' '+c_5min, 40, 40);
+      display_time(c_hour, c_5min, c);
     }
   }, 500);
 })
