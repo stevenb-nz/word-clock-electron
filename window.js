ac5b81a8c803fbcccb12fb1926b6ddb1624b7ec2
Steven Brown <sgbrown@mac.com>
2018-03-30 21:01:28 +1300

    experimentation

--- window.js
+++ window.js
@@ -5,9 +5,11 @@ $(() => {
   function extract_5min (d) {
     return Math.floor(d.getMinutes()/5);
   }
-  function display_time (c_hour, c_5min, c) {
-    c.clearRect(0, 0, canvas.width, canvas.height);
-    c.fillText(c_hour+' '+c_5min*5, 40, 40);
+  function display_5min (c_5min, c) {
+    c.fillText(c_5min*5, 10*c_5min, 40);
+  }
+  function display_hour (c_hour, c) {
+    c.fillText(c_hour, 40, 10*c_hour);
   }
 
   let on_colour = "#FF0000";
@@ -25,18 +27,19 @@ $(() => {
   let d = new Date();
   let c_hour = extract_hour(d);
   let c_5min = extract_5min(d);
-  display_time(c_hour, c_5min, c);
+  display_hour(c_hour, c);
+  display_5min(c_5min, c);
 
   window.setInterval(function() {
     d = new Date();
     if (c_5min !== extract_5min(d)) {
       c.fillStyle = off_colour;
       c_5min = extract_5min(d);
-      display_time(c_hour, c_5min, c);
+      display_5min(c_5min, c);
       if (c_hour !== extract_hour(d)) {
         c.fillStyle = on_colour;
         c_hour = extract_hour(d);
-        display_time(c_hour, c_5min, c);
+        display_hour(c_hour, c);
       }
     }
   }, 500);
